8f0b2a2d1e09c4781e4b0d6ff73fe2141e546a47
Shan.tk <tksudharshan@gmail.com>
2020-05-24 07:24:02 +0530

    Project Page - done

---
+++ lib/art/art_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class ArtPage extends StatefulWidget {
+  @override
+  _ArtPageState createState() => _ArtPageState();
+}
+
+class _ArtPageState extends State<ArtPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
