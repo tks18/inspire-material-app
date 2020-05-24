1b8d44baa7cec370cc3f5fa7438770d5effda9e5
Shan.tk <tksudharshan@gmail.com>
2020-05-24 05:50:12 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/shantk/projects/inspire_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shantk.projects.inspire_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'home/home_page.dart';
+void main() {
+  runApp(MaterialApp(
+    home: HomePage()
+  ));
+}
